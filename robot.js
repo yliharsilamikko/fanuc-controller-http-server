c5ef75af0402e2201b8132f65bcb9d874ff7502f
yliharsilamikko <Mikko.Yliharsila@seamk.fi>
2020-02-13 15:18:25 +0200

    commit

--- robot.js
+++ robot.js
@@ -2,10 +2,15 @@ const create = () => {
     return {
         joints: [0, 0, 0, 0, 0, 0],
         async run() {
-            //while (true) {
-            let time = Date.now() / 1000;
-            this.joints[0] = time;
-            //}
+            while (true) {
+                await new Promise(resolve => setTimeout(resolve, 50));
+                let time = Date.now() / 1000;
+                const amplitude = 90 //deg;
+                const frequency = 0.3; //Hz
+                const joint_value = Math.sin(time * frequency * Math.PI) * amplitude;
+
+                this.joints[0] = joint_value;
+            }
         }
     };
 };
